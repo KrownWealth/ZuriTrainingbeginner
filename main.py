936644728f9ac69aec9a7e8eadd0dae36ee9f398
Adeola Abdulramon <abdulramonadeola36@gmail.com>
2022-05-27 09:39:07 -0700

    Reading-Text-File-Code

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-# Check if two words are anagrams 
+# Read text from a file, and count the occurence of words in that text
 # Example:
-# find_anagrams("hello", "check") --> False
-# find_anagrams("below", "elbow") --> True
+# count_words("The cake is done. It is a big cake!") 
+# --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
 
-def are_anagram1(string1, string2):
+def read_file_content(filename):
+    # [assignment] Add your code here 
+    g = open("story.txt", "r")
+    Training = g.read()
+    g.close()
+    return(Training)
+
+kaf = read_file_content('filename')
+print(kaf)
+
+
+def count_words():
+    text = read_file_content("./story.txt")
     # [assignment] Add your code here
-    return sorted(string1) == sorted(string2)
+    count = dict()
+    words = text.split()
+    for word in words:
+        if word in count:
+            count[word] += 1
+        else:
+        
+            count[word] = 1
+    return(count)
+print(count_words())    
 
-print(are_anagram1("secure", "rescue"))
 
+    #return {"as": 10, "would": 20}
